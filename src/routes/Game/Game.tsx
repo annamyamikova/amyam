6797ac5d3d44fd4829ae744e1ec00e94b354da9d
annamyamikova <myamikova@gmail.com>
2023-01-19 15:18:21 +0600

    Character: add move event

--- src/routes/Game/Game.tsx
+++ src/routes/Game/Game.tsx
@@ -1,10 +1,17 @@
 import React, { FC } from 'react';
 
+// Components
+import Character from 'components/Character';
+
 // Styles
 import classes from './Game.module.scss';
 
 const Game: FC = () => {
-  return <div className={classes.Container} />;
+  return (
+    <div className={classes.Container}>
+      <Character />
+    </div>
+  );
 };
 
 export default Game;
